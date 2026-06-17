69b5b38ee50ded7f2509a8d52b2ba16a852b0164
Mose Odhiambo <moseodhiambo26@gmail.com>
2026-06-17 16:57:33 +0300

    auto create superuser

--- core/wsgi.py
+++ core/wsgi.py
@@ -14,3 +14,26 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
+
+
+# ✅ AUTO CREATE SUPERUSER
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if username and password:
+        if not User.objects.filter(username=username).exists():
+            print("Creating superuser...")
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+        else:
+            print("Superuser already exists.")
+except Exception as e:
+    print("Error creating superuser:", e)
