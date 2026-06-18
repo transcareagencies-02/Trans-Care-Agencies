5e5833bc65d6ae5e5ee92c883d2b9d94b5cb7fab
Mose Odhiambo <moseodhiambo26@gmail.com>
2026-06-18 14:02:23 +0300

    Fix storage config

--- core/settings.py
+++ core/settings.py
@@ -145,7 +145,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
 
